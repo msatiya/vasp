887afcc16000030b2b0ae5df32aa41bca562f7b4
Satiyaboshan Murugaboopathy <murugaboopathysatiyabooshan@gmail.com>
2023-05-05 12:43:30 +0000

    netflix training_logs

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-nohup python vasp_ml_script.py > vasp_ml_script.out &
-#nohup python vasp_netflix_script.py > vasp_netflix_script.out
+#nohup python vasp_ml_script.py > vasp_ml_script.out &
+nohup python vasp_netflix_script.py > vasp_netflix_script.out
 #nohup python vasp_steam_script.py > vasp_steam_script.out
